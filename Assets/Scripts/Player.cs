0ac4acb1376ff9a265870830be6144bc684203aa
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-24 21:26:52 +0700

    Remove unwanted stuff from script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,9 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float movespeed = 5f;
     [SerializeField] private float rotatespeed = 8f;
-	[SerializeField] public Material selected;
-	[SerializeField] public Material unselected;
 
     private bool isWalking;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -36,18 +28,11 @@ public class Player : MonoBehaviour
 
 	private void OnCollisionEnter(Collision collision)
 	{
-		if(collision.gameObject.tag == "Counter")
-		{
-			collision.gameObject.GetComponentInChildren<MeshRenderer>().material = selected;
-			Debug.Log(collision.gameObject.name);
-		}
+		//
 	}
 
 	private void OnCollisionExit(Collision collision)
 	{
-		if(collision.gameObject.tag == "Counter")
-		{
-			collision.gameObject.GetComponentInChildren<MeshRenderer>().material = unselected;
-		}
+		//
 	}
 }
