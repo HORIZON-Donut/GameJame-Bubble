ebb0a4cb31a2e517dfc0ec14698d217975f9cd28
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-25 11:24:19 +0700

    Make player rotate on mouse move at horizontal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,15 +11,20 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
+		Vector2 mouseInput = new Vector2(0, 0);
 
 		inputVector.y = Input.GetAxisRaw("Vertical");
 		inputVector.x = Input.GetAxisRaw("Horizontal");
         inputVector = inputVector.normalized;
 
+		mouseInput.x = Input.GetAxisRaw("Mouse X");
+		mouseInput.y = Input.GetAxisRaw("Mouse Y");
+		mouseInput = mouseInput.normalized;
+
         Vector3 moveDir = new Vector3(transform.forward.x, 0f, transform.forward.z) * inputVector.y;
 
         transform.position += moveDir * movespeed * Time.deltaTime;
-		transform.Rotate(0f, inputVector.x*rotatespeed, 0f);
+		transform.Rotate(0f, mouseInput.x*rotatespeed, 0f);
 
         isWalking = moveDir != Vector3.zero;
 
