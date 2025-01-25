e3d8180455bd9dd79d0e194a869635db6ff9e607
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-25 19:57:47 +0700

    Add some stuff ti inventory and also define arm weapon function

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Inventory: MonoBehaviour
 {
 	[SerializeField] private List<WeaponItem> WeaponList = new List<WeaponItem>();
+	
+	private int total = 0;
+	private int weaponIndex = 0;
 
 	void Update()
 	{
+		weaponIndex += 1;
+		total = WeaponList.Count;
+		weaponIndex = weaponTndex%total;
+		
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
-			Debug.Log("Change Weapon");
+			//
 		}
+	}
+
+	void ArmWeapon()
+	{
 	}
 }
