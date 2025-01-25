26814f04f1f9fc63e453eed3444befca161c1ba4
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-25 16:52:31 +0700

    Update weaponSO script

--- Assets/Scripts/weaponSO.cs
+++ Assets/Scripts/weaponSO.cs
@@ -7,4 +7,7 @@ public class  WeaponSO: ScriptableObject
 	public string WeapomName;
 	public int BulletNumber;
 	public float Damage;
+	public float FireRate;
+	public gameObject bullet;
+	public gameObject Body;
 }
