9ddccb187d79ea03b355d6ba9145b3223ea614ef
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-25 16:21:51 +0700

    Assign weeapon so script

---
+++ Assets/Scripts/weaponSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(filaName = "New Weapon", memuName = "WeaponSO)]
+class public WeaponSO: ScriptableObject
+{
+	public String WeapomName;
+	public int BulletNumber;
+	public float Damage;
+}
