19005729c73955b15cb22d99311f15cbb0211dba
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-26 11:07:29 +0700

    Move stuff in update function into standalone function

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -38,50 +38,57 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+		Walk();
+		Rotate();
+		Jump();
+    }
+
+	private void Walk()
+	{
         Vector2 inputVector = new Vector2(0, 0);
-		Vector2 mouseInput = new Vector2(0, 0);
 
 		inputVector.y = Input.GetAxisRaw("Vertical");
 		inputVector.x = Input.GetAxisRaw("Horizontal");
         inputVector = inputVector.normalized;
-
-		mouseInput.x = Input.GetAxisRaw("Mouse X");
-		mouseInput.y = -Input.GetAxisRaw("Mouse Y");
-		mouseInput = mouseInput.normalized;
-
-		if(Input.GetKey(KeyCode.LeftShift))
+		
+		if(isGround)
 		{
-			speed = spritspeed;
-		}
-		else if(Input.GetKey(KeyCode.LeftShift) && !isGround)
-		{
-			speed = midairspeed;
+			speed = Input.GetKey(KeyCode.LeftShift) ? spritspeed : movespeed;
 		}
 		else
 		{
-			speed = movespeed;
-		}
-
-		if(Input.GetKey(KeyCode.Space) && isGround)
-		{
-			Jump();
+			speed = Input.GetKey(KeyCode.E) ? midairspeed : movespeed;
 		}
 
         Vector3 moveDir = (new Vector3(transform.forward.x, 0f, transform.forward.z) * inputVector.y) + (transform.right * inputVector.x);
 
         transform.position += moveDir * speed * Time.deltaTime;
+	}
+
+	private void Rotate()
+	{
+		Vector2 mouseInput = new Vector2(0, 0);
+
+		mouseInput.x = Input.GetAxisRaw("Mouse X");
+		mouseInput.y = -Input.GetAxisRaw("Mouse Y");
+		mouseInput = mouseInput.normalized;
+
 		transform.Rotate(0f, mouseInput.x*rotatespeed, 0f);
 
 		float newRotation = Mathf.Clamp(rotX + mouseInput.y * rotatespeed, -maxAngle, maxAngle);
 		Head.transform.localRotation = Quaternion.Euler(newRotation, 0f, 0f);
 
 		rotX = newRotation;
-    }
+
+	}
 
 	private void Jump()
 	{
-		rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
-		isGround = false;
+		if(Input.GetKeyDown(KeyCode.Space) && isGround)
+		{
+			rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+			isGround = false;
+		}
 	}
 
 	private void OnCollisionEnter(Collision collision)
